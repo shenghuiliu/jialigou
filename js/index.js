81df8da62b7ccb1264db19c036e27285aaae621c
easyhoon <1062101686@qq.com>
2017-11-14 10:26:56 +0800

    新增图片

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(function(){
+	$(".topAll .topAllCenter a").hover(function(){
+		$(this).css({"color":"#e91456","text-decoration":"underline"})
+	},function(){
+		$(this).css({"color":"#333","text-decoration":"none"})
+	})
+	
+	$(".topAll .topAllRight a").hover(function(){
+		$(this).css({"color":"#e91456","text-decoration":"underline"})
+	},function(){
+		$(this).css({"color":"#333","text-decoration":"none"})
+	})
+})
